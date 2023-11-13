f7d4ff7f54a1795d660b07547b24291442cc0839
festoney8 <festoney8@gmail.com>
2023-11-13 11:36:32 +0800

    feat: complete panel css and add some item snippet

--- bilibili-cleaner.js
+++ bilibili-cleaner.js
@@ -16,7 +16,7 @@
     'use strict'
 
     class Group {
-        // Group名称，描述，启用Group的域名模式，item数组
+        // Group id，描述，启用Group的域名模式，item数组
         constructor(groupID, description, mode, items) {
             this.groupID = groupID
             this.description = description
@@ -31,7 +31,7 @@
                 <div class="bili-cleaner-group-title">
                 </div>
                 <hr>
-                <div class="bili-cleaner-itemlist">
+                <div class="bili-cleaner-item-list">
                 </div>
             </div>`
             e.querySelector('.bili-cleaner-group').id = this.groupID
@@ -60,7 +60,6 @@
             const url = location.href
             const pathname = location.pathname
             console.log("enableGroup", host, url, pathname)
-            console.log(this)
             if (this.mode === "common") {
                 this.enableItems()
             } else if (host == 't.bilibili.com' && this.mode == 'trends') {
@@ -81,7 +80,7 @@
         uncheckedHTML = `<input class="bili-cleaner-item-switch" type="checkbox">`
         checkedHTML = `<input class="bili-cleaner-item-switch" type="checkbox" checked>`
 
-        // item名称，所属group，功能描述，功能function，功能CSS
+        // item id，所属group，功能描述，功能function，功能CSS
         constructor(itemID, groupID, description, itemFunc, itemCSS) {
             this.itemID = itemID
             this.description = description
@@ -97,18 +96,20 @@
         setStatus(value) {
             this.isEnable = GM_setValue(this.itemID, value)
         }
-        // 在panel的对应group内添加item项目
+        // 在相应group内添加item
         insertItem() {
             this.getStatus()
             const e = document.createElement('label')
             e.id = this.itemID
             if (this.isEnable) {
-                e.innerHTML = this.checkedHTML + '\t' + this.description
+                e.innerHTML = this.checkedHTML + '　' + this.description
             } else {
-                e.innerHTML = this.uncheckedHTML + '\t' + this.description
+                e.innerHTML = this.uncheckedHTML + '　' + this.description
             }
-            const itemGroup = document.querySelector(`#${this.groupID} .bili-cleaner-itemlist`)
-            itemGroup.appendChild(e)
+            const itemGroup = document.querySelector(`#${this.groupID} .bili-cleaner-item-list`)
+            if (itemGroup) {
+                itemGroup.appendChild(e)
+            }
         }
         // 监听item check状态
         watchItem() {
@@ -116,11 +117,9 @@
             this.HTMLElement.addEventListener('change', (event) => {
                 // this指向class, this.checked无效, 使用event.target访问checkbox
                 if (event.target.checked) {
-                    console.log(this.itemID, 'checked')
                     this.setStatus(true)
                     this.insertItemCSS()
                 } else {
-                    console.log(this.itemID, 'not checked')
                     this.setStatus(false)
                     this.removeItemCSS()
                 }
@@ -129,21 +128,25 @@
         // 启用CSS片段
         insertItemCSS() {
             if (this.itemCSS) {
+                // check if CSS exist
+                const isExist = document.querySelector(`style[bili-cleaner-css-item=${this.itemID}]`)
+                if (isExist) {
+                    return
+                }
+
                 const style = document.createElement('style')
                 style.innerText = this.itemCSS
                 // 指定CSS片段ID，用于实时启用停用
-                style.setAttribute('bili-cleaner-css-itemID', this.itemID)
+                style.setAttribute('bili-cleaner-css-item', this.itemID)
                 console.log('insertItemCSS')
-                console.log(style)
                 document.head.appendChild(style)
             }
         }
         // 停用CSS片段
         removeItemCSS() {
             if (this.itemCSS) {
-                const style = document.querySelector(`style[bili-cleaner-css-itemID=${this.itemID}]`)
+                const style = document.querySelector(`style[bili-cleaner-css-item=${this.itemID}]`)
                 console.log('removeItemCSS')
-                console.log(style)
                 if (style) {
                     style.parentNode.removeChild(style)
                 }
@@ -152,11 +155,11 @@
         // 执行功能（由group调用）
         enableItem() {
             console.log("enableItem")
-            console.log(this)
             this.getStatus()
             if (this.isEnable) {
                 this.insertItemCSS()
                 if (this.itemFunc instanceof Function) {
+                    console.log(this.isEnable, this.itemFunc)
                     this.itemFunc()
                 }
             }
@@ -171,26 +174,27 @@
             left: 50%;
             top: 50%;
             transform: translate(-50%, -50%);
-            width: 30vw;
+            width: 32vw;
             height: 90vh;
             border-radius: 10px;
-            overflow: hidden;
             background: rgba(250, 250, 250, 1);
-            border: 2px solid rgba(251, 114, 153, 1);
+            box-shadow: 0 2px 5px rgba(251, 114, 153, 1);
+            overflow: auto;
             z-index: 2147483647;
         }
-        
+
         #bili-cleaner-bar {
-            width: 30vw;
+            width: 32vw;
             height: 6vh;
             background: rgba(251, 114, 153, 1);
-            position: relative;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
             cursor: move;
             user-select: none;
         }
         
         #bili-cleaner-title {
-            width: 30vw;
+            width: 32vw;
             height: 6vh;
             display: flex;
             justify-content: center;
@@ -224,21 +228,32 @@
         #bili-cleaner-close svg {
             text-align: center;
         }
+
+        #bili-cleaner-group-list {
+            height: 84vh;
+            overflow: auto;
+        }
+
+        #bili-cleaner-group-list::-webkit-scrollbar {
+            display: none;
+        }
         
         /* panel内的group */
         .bili-cleaner-group {
             margin: 10px;
             background: white;
-            border-radius: 10px;
-            padding: 5px 10px;
-            box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);
+            border-radius: 5px;
+            padding: 5px 15px;
+            box-shadow: 0 0 3px rgba(0, 0, 0, 0.15);
+            user-select: none;
         }
+        
         .bili-cleaner-group hr {
             border: 1px solid #f3f3f3;
         }
 
         .bili-cleaner-group-title {
-            font-size: 1.2em;
+            font-size: 1.25em;
             font-weight: bold;
             padding: 2px;
         }
@@ -247,12 +262,14 @@
             padding: 2px;
         }
 
+        /* 每行选项的样式, 按钮和文字 */
         .bili-cleaner-item-list label {
             display: block;
             vertical-align: middle;
+            margin: 8px 0;
+            font-size: 1.2em;
         }
         
-        /* 每行选项的样式, 按钮和文字 */
         .bili-cleaner-item-switch {
             vertical-align: middle;
             width: 50px;
@@ -350,29 +367,229 @@
 
     /////////////////////////////////////////////////////////////////////////////////////////
     const GROUPS = []
-
+    // 首页
     const homepageItems = []
+    // 通用
     const commonItems = []
+    // 播放页
     const videoItems = []
+    // 搜索页
     const searchItems = []
+    // 动态页
     const trendsItems = []
+    // 直播页
     const liveItems = []
 
     homepageItems.push(
-        new Item('hideHomepageSubarea', 'homepage', '隐藏分区', null,
-            `.bili-header__channel {
+        new Item(
+            'hideSubarea',
+            'bili-cleaner-homepage',
+            '隐藏整个分区栏',
+            null,
+            `
+            .bili-header__channel .channel-icons {
                 display: none !important;
-            }`)
+            }
+            .bili-header__channel .right-channel-container {
+                display: none !important;
+            }
+            .bili-header__channel {
+                height: 20px !important;
+            }
+            `
+        )
     )
-    commonItems.push(
 
+    homepageItems.push(
+        new Item(
+            'hideRecommendSwipe',
+            'bili-cleaner-homepage',
+            '隐藏大图活动轮播',
+            null,
+            `
+            .recommended-swipe {
+                display: none;
+            }
+            .recommended-container_floor-aside .container>*:nth-of-type(n + 8) {
+                margin-top: 0 !important;
+            }
+            .recommended-container_floor-aside .container .feed-card:nth-of-type(n + 9) {
+                display: inherit !important;
+            }
+            .recommended-container_floor-aside .container.is-version8>*:nth-of-type(n + 13) {
+                margin-top: 0 !important;
+            }
+            .recommended-container_floor-aside .container .feed-card:nth-of-type(n + 12) {
+                display: inherit !important;
+            }
+            .recommended-container_floor-aside .container>*:nth-of-type(n + 6) {
+                margin-top: 0 !important;
+            }
+            `
+        )
     )
-    GROUPS.push(new Group('common', '通用', 'common', commonItems))
-    GROUPS.push(new Group('homepage', '首页', 'homepage', homepageItems))
-    GROUPS.push(new Group('common', '播放页', 'common', videoItems))
-    GROUPS.push(new Group('common', '搜索页', 'common', searchItems))
-    GROUPS.push(new Group('common', '动态页', 'common', trendsItems))
-    GROUPS.push(new Group('common', '直播页', 'common', liveItems))
+
+    homepageItems.push(
+        new Item(
+            'hideStickySubarea',
+            'bili-cleaner-homepage',
+            '隐藏滚动页面时顶部sticky分区栏',
+            null,
+            `
+            .header-channel {
+                display: none;
+            }
+            `
+        )
+    )
+
+    homepageItems.push(
+        new Item(
+            'hideUPInfoIcon',
+            'bili-cleaner-homepage',
+            '隐藏推荐视频 "已关注" "10万点赞" 小图标',
+            null,
+            `
+            .bili-video-card .bili-video-card__info--icon-text {
+                display: none;
+            }
+            `
+        )
+    )
+    homepageItems.push(
+        new Item(
+            'hideDanmakuCount',
+            'bili-cleaner-homepage',
+            '隐藏视频弹幕数显示',
+            null,
+            `
+            .bili-video-card__stats--item:nth-child(2) {
+                visibility: hidden;
+            }
+            `
+        )
+    )
+    homepageItems.push(
+        new Item(
+            'hideVideoInfoDate',
+            'bili-cleaner-homepage',
+            '隐藏视频发布日期',
+            null,
+            `
+            .bili-video-card__info--date {
+                display: none;
+            }
+            `
+        )
+    )
+    homepageItems.push(
+        new Item(
+            'hideAd',
+            'bili-cleaner-homepage',
+            '隐藏推荐视频中的广告',
+            null,
+            `
+            .feed-card:has(.bili-video-card__info--ad) {
+                display: none;
+            }
+            .recommended-container_floor-aside .container>*:nth-of-type(n + 8) {
+                margin-top: 0 !important;
+            }
+            .recommended-container_floor-aside .container .feed-card:nth-of-type(n + 9) {
+                display: inherit !important;
+            }
+            .recommended-container_floor-aside .container.is-version8>*:nth-of-type(n + 13) {
+                margin-top: 0 !important;
+            }
+            .recommended-container_floor-aside .container .feed-card:nth-of-type(n + 12) {
+                display: inherit !important;
+            }
+            .recommended-container_floor-aside .container>*:nth-of-type(n + 6) {
+                margin-top: 0 !important;
+            }
+            `
+        )
+    )
+    // BV号转AV号
+    function bv2av() {
+        // algo by mcfx, https://www.zhihu.com/question/381784377/answer/1099438784
+        function dec(x) {
+            let table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF';
+            let tr = {};
+            for (let i = 0; i < 58; i++) {
+                tr[table[i]] = i;
+            }
+            let s = [11, 10, 3, 8, 4, 6];
+            let xor = 177451812;
+            let add = 8728348608;
+            let r = 0;
+            for (let i = 0; i < 6; i++) {
+                r += tr[x[s[i]]] * 58 ** i;
+            }
+            return (r - add) ^ xor;
+        }
+
+        if (location.href.includes('bilibili.com/video/BV')) {
+            let regex = /bilibili.com\/video\/(BV[0-9a-zA-Z]+)/;
+            let match = regex.exec(location.href);
+            if (match) {
+                let aid = dec(match[1]);
+                let newURL = `https://www.bilibili.com/video/av${aid}`;
+                if (location.hash.slice(1, 6) === 'reply') {
+                    newURL += location.hash;
+                }
+                history.replaceState(null, null, newURL);
+            }
+        }
+    }
+
+    videoItems.push(
+        new Item(
+            'videoPageBv2av',
+            'bili-cleaner-video',
+            'BV号转AV号(需刷新)',
+            bv2av,
+            null
+        )
+    )
+
+    // 移除URL中的跟踪参数
+    function removeQueryParams() {
+        let keysToRemove = ['from_source', 'spm_id_from', 'search_source', 'vd_source', 'unique_k', 'is_story_h5', 'from_spmid',
+            'share_plat', 'share_medium', 'share_from', 'share_source', 'share_tag', 'up_id', 'timestamp', 'mid',
+            'live_from', 'launch_id', 'session_id'];
+
+        let url = location.href;
+        let urlObj = new URL(url);
+        let params = new URLSearchParams(urlObj.search);
+
+        keysToRemove.forEach(function (key) {
+            params.delete(key);
+        });
+
+        urlObj.search = params.toString();
+        let newUrl = urlObj.toString();
+        if (newUrl !== url) {
+            history.replaceState(null, null, newUrl);
+        }
+    }
+    commonItems.push(
+        new Item(
+            'urlCleaner',
+            'bili-cleaner-common',
+            'URL参数净化(需刷新)',
+            removeQueryParams,
+            null
+        )
+    )
+
+
+    GROUPS.push(new Group('bili-cleaner-common', '通用', 'common', commonItems))
+    GROUPS.push(new Group('bili-cleaner-homepage', '首页', 'homepage', homepageItems))
+    GROUPS.push(new Group('bili-cleaner-video', '播放页', 'video', videoItems))
+    GROUPS.push(new Group('bili-cleaner-search', '搜索页', 'search', searchItems))
+    GROUPS.push(new Group('bili-cleaner-trends', '动态页', 'trends', trendsItems))
+    GROUPS.push(new Group('bili-cleaner-live', '直播页', 'live', liveItems))
 
     GROUPS.forEach(e => { e.enableGroup() })
 
@@ -382,4 +599,15 @@
         e.insertGroup()
         e.insertItems()
     })
+    ////////////////////////////////////////////////////////////////////////////////////
+    function openSettings() {
+        addGlobalCSS()
+        createPanel()
+        GROUPS.forEach(e => {
+            e.insertGroup()
+            e.insertItems()
+        })
+    }
+    GM_registerMenuCommand("设置", openSettings);
+
 })();

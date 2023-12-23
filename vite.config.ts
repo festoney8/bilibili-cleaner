61a31881ddba57bc8b12e89589788ad4a9a06f25
festoney8 <festoney8@gmail.com>
2023-12-23 14:43:14 +0800

    fix: fix config

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
             userscript: {
                 icon: 'https://vitejs.dev/logo.svg',
                 namespace: 'npm/vite-plugin-monkey',
-                match: ['https://www.google.com/'],
+                match: ['*://*.bilibili.com/*'],
             },
         }),
     ],

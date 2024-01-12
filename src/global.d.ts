bd08251c9a4cbbb32bb551f50c11f7d26ae503f0
festoney8 <festoney8@gmail.com>
2024-01-13 05:35:25 +0800

    optimize: override player height when hide sending area

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,9 @@
 export declare global {
     type myCSS = string
     type myHTML = string
+    interface Window {
+        isWide?: boolean
+        hasBlackSide?: boolean
+        setSize?: function
+    }
 }

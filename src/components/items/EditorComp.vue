82695b2bc755dc2db88a11050b6bda5a4a259479
festoney8 <festoney8@gmail.com>
2024-09-27 18:18:59 +0800

    update: item comp

--- src/components/items/EditorComp.vue
+++ src/components/items/EditorComp.vue
@@ -9,7 +9,7 @@
         </button>
         <div class="ml-2 self-center text-black">{{ name }}</div>
     </div>
-    <DescriptionComp class="pl-12" v-if="description?.length" :description="description"></DescriptionComp>
+    <DescriptionComp class="pl-10" v-if="description?.length" :description="description"></DescriptionComp>
 
     <PanelComp
         ref="panel"
@@ -31,18 +31,20 @@
             ></DescriptionComp>
             <textarea
                 v-model="editorData"
-                class="flex-1 resize-none overscroll-none rounded-md border-2 border-gray-300 p-2 leading-snug outline-none focus:border-gray-400"
+                class="flex-1 resize-none overscroll-none rounded-md border-2 border-gray-300 p-2 text-[15px] outline-none focus:border-gray-400"
+                style="scrollbar-width: thin; scrollbar-color: #999 #00000000"
                 placeholder="请输入内容..."
             ></textarea>
             <div class="mt-4 flex justify-around">
                 <button
-                    class="w-24 self-center rounded-md border-2 border-gray-300 bg-white py-0.5 text-center text-lg hover:border-blue-400"
+                    class="w-24 self-center rounded-md border-2 border-gray-300 bg-white py-0.5 text-center text-lg hover:border-blue-400 hover:bg-blue-50"
+                    :class="saveSuccess ? 'border-green-400 bg-green-50 hover:border-green-400 hover:bg-green-50' : ''"
                     @click="saveData"
                 >
                     保存
                 </button>
                 <button
-                    class="w-24 self-center rounded-md border-2 border-gray-300 bg-white py-0.5 text-center text-lg hover:border-red-300"
+                    class="w-24 self-center rounded-md border-2 border-gray-300 bg-white py-0.5 text-center text-lg hover:border-red-300 hover:bg-red-50"
                     @click="isEditorShow = false"
                 >
                     关闭
@@ -61,21 +63,25 @@ import DescriptionComp from './DescriptionComp.vue'
 
 const item = defineProps<IEditorItem>()
 
-const isEditorShow = ref(false)
-
-const editorData = ref(GM_getValue(item.id, []).join('\n'))
-
 const panel = ref<HTMLElement | null>(null)
+
+const isEditorShow = ref(false)
+const saveSuccess = ref(false)
+const editorData = ref(GM_getValue(item.id, []).join('\n') + '\n') // 末尾空行
 
 const saveData = () => {
     const data = editorData.value.split('\n').filter((v) => v.trim() !== '')
     GM_setValue(item.id, data)
+    saveSuccess.value = true
+    setTimeout(() => {
+        saveSuccess.value = false
+    }, 1500)
     item.saveFn()
 }
 
 onBeforeUpdate(() => {
     if (panel.value) {
-        editorData.value = GM_getValue(item.id, []).join('\n')
+        editorData.value = GM_getValue(item.id, []).join('\n') + '\n'
     }
 })
 </script>
--- src/components/items/SwitchComp.vue
+++ src/components/items/SwitchComp.vue
@@ -1,17 +1,19 @@
 <template>
     <SwitchGroup class="m-0.5 h-fit w-full rounded-lg py-1 hover:bg-blue-50 hover:bg-opacity-50">
         <div class="flex items-center">
-            <Switch
-                v-model="enabled"
-                :class="enabled ? 'bg-[#00AEEC]' : 'bg-gray-200'"
-                class="relative inline-flex h-6 w-11 items-center rounded-full outline-none transition-colors"
-            >
-                <span
-                    :class="enabled ? 'translate-x-6' : 'translate-x-1'"
-                    class="inline-block h-4 w-4 transform rounded-full bg-white transition-transform"
-                />
-            </Switch>
-            <SwitchLabel class="ml-2 flex-1 text-black"> {{ name }}</SwitchLabel>
+            <SwitchLabel class="flex flex-1 flex-row text-black">
+                <Switch
+                    v-model="enabled"
+                    :class="enabled ? 'bg-[#00AEEC]' : 'bg-gray-200'"
+                    class="relative inline-flex h-6 w-11 items-center rounded-full outline-none transition-colors"
+                >
+                    <span
+                        :class="enabled ? 'translate-x-6' : 'translate-x-1'"
+                        class="inline-block h-4 w-4 transform rounded-full bg-white transition-transform"
+                    />
+                </Switch>
+                <p class="ml-2 flex-1">{{ name }}</p>
+            </SwitchLabel>
         </div>
     </SwitchGroup>
     <DescriptionComp class="pl-10" v-if="description?.length" :description="description"></DescriptionComp>

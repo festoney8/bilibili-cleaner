85099b6f7a696a957e78cf12e13a0e4324d40b4c
festoney8 <festoney8@gmail.com>
2024-10-21 07:04:01 +0800

    update: item details

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,29 +1,7 @@
 export declare global {
     interface Window {
         isWide?: boolean
-        hasBlackSide?: boolean
-        setSize?: function
-        webAbTest?: {
-            danmuku_block_version?: string
-        }
-        vd?: {
-            aid?: number
-            bvid?: string
-        }
-        player?: {
-            __core: () => {
-                uiStore: {
-                    state: {
-                        miniScreenBottom: number
-                        miniScreenRight: number
-                    }
-                }
-            }
-        }
         __INITIAL_STATE__?: {
-            abtest?: {
-                comment_next_version?: 'ELEMENTS' | 'DEFAULT'
-            }
             related?: {
                 bvid?: string
             }[]

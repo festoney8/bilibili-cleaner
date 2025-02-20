075b915121ba24ec7164a01805251b39e4cd1476
festoney8 <festoney8@gmail.com>
2025-02-20 17:44:14 +0800

    revert: hide header search rcmd (#199)

--- src/modules/rules/common/groups/headerCenter.ts
+++ src/modules/rules/common/groups/headerCenter.ts
@@ -10,29 +10,31 @@ export const commonHeaderCenterItems: Item[] = [
         type: 'switch',
         id: 'common-hide-nav-search-rcmd',
         name: '隐藏 推荐搜索',
-        enableFn: async () => {
-            let cnt = 0
-            const id = setInterval(() => {
-                const el = document.querySelector('input.nav-search-input') as HTMLInputElement
-                if (el) {
-                    clearInterval(id)
-                    el.title = ''
-                    el.placeholder = ''
-                    new MutationObserver(() => {
-                        if (el.title) {
-                            el.title = ''
-                        }
-                        if (el.placeholder) {
-                            el.placeholder = ''
-                        }
-                    }).observe(el, {
-                        attributeFilter: ['placeholder', 'title'],
-                    })
-                }
-                ++cnt > 20 && clearInterval(id)
-            }, 500)
-        },
-        enableFnRunAt: 'document-end',
+        // https://greasyfork.org/zh-CN/scripts/479861/discussions/285183
+
+        // enableFn: async () => {
+        //     let cnt = 0
+        //     const id = setInterval(() => {
+        //         const el = document.querySelector('input.nav-search-input') as HTMLInputElement
+        //         if (el) {
+        //             clearInterval(id)
+        //             el.title = ''
+        //             el.placeholder = ''
+        //             new MutationObserver(() => {
+        //                 if (el.title) {
+        //                     el.title = ''
+        //                 }
+        //                 if (el.placeholder) {
+        //                     el.placeholder = ''
+        //                 }
+        //             }).observe(el, {
+        //                 attributeFilter: ['placeholder', 'title'],
+        //             })
+        //         }
+        //         ++cnt > 20 && clearInterval(id)
+        //     }, 500)
+        // },
+        // enableFnRunAt: 'document-end',
     },
     {
         type: 'switch',

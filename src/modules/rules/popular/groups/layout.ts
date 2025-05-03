ad2f3545281d933f605719913d9d94eabd61d711
festoney8 <festoney8@gmail.com>
2025-05-04 02:06:35 +0800

    update: optimize popular layout style

--- src/modules/rules/popular/groups/layout.ts
+++ src/modules/rules/popular/groups/layout.ts
@@ -7,11 +7,7 @@ export const popularLayoutItems: Item[] = [
         name: '强制修改视频列数',
         defaultValue: 'popular-layout-2-column',
         disableValue: 'popular-layout-disable',
-        description: [
-            '默认隐藏视频简介、标签',
-            '对 综合热门/每周必看/入站必刷/排行榜 生效',
-            '使用 5 列或 6 列布局时，建议开启 "隐藏 弹幕数"',
-        ],
+        description: ['默认隐藏视频简介、标签', '对 全站音乐榜 不生效'],
         options: [
             {
                 id: 'popular-layout-2-column',

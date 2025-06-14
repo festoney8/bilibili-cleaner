e48b2fd8efec52eb16be44a90eadc79454124be4
festoney8 <festoney8@gmail.com>
2025-06-14 17:16:52 +0800

    update: core checker

--- src/modules/filters/core/core.ts
+++ src/modules/filters/core/core.ts
@@ -1,7 +1,11 @@
 import settings from '@/settings'
 import { SubFilterPair } from '@/types/filter'
-import { error } from '@/utils/logger'
 import { hideEle, showEle } from '@/utils/tool'
+import { useThrottleFn } from '@vueuse/core'
+import pLimit from 'p-limit'
+
+// 限制并发
+const limit = pLimit(10)
 
 /**
  * 检测元素列表中每个元素是否合法, 隐藏不合法的元素
@@ -11,56 +15,49 @@ import { hideEle, showEle } from '@/utils/tool'
  * @param whitePairs 白名单过滤器与使用的选择函数列表
  * @param sign 是否标记已检测过
  */
-export const coreCheck = async (
-    elements: HTMLElement[],
-    sign = true,
-    blackPairs: SubFilterPair[],
-    whitePairs?: SubFilterPair[],
-): Promise<number> => {
-    let blackCnt = 0
-    try {
-        // 构建黑白名单检测任务
-        for (const el of elements) {
-            const blackTasks: Promise<void>[] = []
-            blackPairs.forEach((pair) => {
-                blackTasks.push(pair[0].check(el, pair[1]))
-            })
+export const coreCheck = useThrottleFn(
+    async (
+        elements: HTMLElement[],
+        sign = true,
+        blackPairs: SubFilterPair[],
+        whitePairs?: SubFilterPair[],
+    ): Promise<number> => {
+        const toHideIdx = new Set<number>()
 
-            await Promise.all(blackTasks)
-                .then(() => {
-                    // 未命中黑名单
-                    showEle(el)
+        const tasks = elements.map((el, idx) =>
+            limit(async () => {
+                const blackTasks: Promise<void>[] = []
+                blackPairs.forEach((pair) => {
+                    blackTasks.push(pair[0].check(el, pair[1]))
                 })
-                .catch(() => {
+                await Promise.all(blackTasks).catch(async () => {
                     // 命中黑名单，构建白名单任务
                     const whiteTasks: Promise<void>[] = []
                     whitePairs?.forEach((pair) => {
                         whiteTasks.push(pair[0].check(el, pair[1]))
                     })
                     if (whiteTasks.length) {
-                        Promise.all(whiteTasks)
-                            .then(() => {
-                                // 命中黑名单，未命中白名单
-                                hideEle(el)
-                                blackCnt++
-                            })
-                            .catch(() => {
-                                // 命中白名单
-                                showEle(el)
-                            })
+                        await Promise.all(whiteTasks).then(() => {
+                            // 命中黑名单，未命中白名单
+                            toHideIdx.add(idx)
+                        })
                     } else {
-                        hideEle(el)
-                        blackCnt++
+                        toHideIdx.add(idx)
                     }
                 })
+            }),
+        )
 
-            // 标记已过滤元素
-            if (sign) {
-                el.setAttribute(settings.filterSign, '')
-            }
-        }
-    } catch (err) {
-        error('coreCheck error', err)
-    }
-    return blackCnt
-}
+        await Promise.all(tasks).finally(() => {
+            // 隐藏元素、标记已访问
+            requestAnimationFrame(() => {
+                for (let i = 0; i < elements.length; i++) {
+                    toHideIdx.has(i) ? hideEle(elements[i]) : showEle(elements[i])
+                    sign && elements[i].setAttribute(settings.filterSign, '')
+                }
+            })
+        })
+        return toHideIdx.size
+    },
+    50,
+)

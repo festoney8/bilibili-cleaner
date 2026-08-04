5a7154bacfed3dabba1675407914248207edb143
festoney8 <festoney8@gmail.com>
2026-08-05 03:30:00 +0800

    fix: hide nav search rcmd

--- src/modules/rules/live/groups/headerCenter.ts
+++ src/modules/rules/live/groups/headerCenter.ts
@@ -1,10 +1,7 @@
 import { Item } from '@/types/item'
-import { createNavSearchRcmdHider } from '@/utils/tool'
+import { isPageLive } from '@/utils/pageType'
 
-const liveNavSearchRcmdHider = createNavSearchRcmdHider(
-    '#nav-searchform input, input.nav-search-content',
-    '#nav-searchform .search-btn, #nav-searchform .nav-search-btn',
-)
+let isRcmdHide = false
 
 export const liveHeaderCenterItems: Item[] = [
     {
@@ -16,39 +13,66 @@ export const liveHeaderCenterItems: Item[] = [
         type: 'switch',
         id: 'live-page-nav-search-rcmd',
         name: '隐藏 推荐搜索',
+        description: ['禁用后需刷新页面'],
         // https://greasyfork.org/zh-CN/scripts/479861/discussions/285183
-
-        // enableFn: async () => {
-        //     let cnt = 0
-        //     const id = setInterval(() => {
-        //         const el = document.querySelector('input.nav-search-content') as HTMLInputElement
-        //         if (el) {
-        //             clearInterval(id)
-        //             el.title = ''
-        //             el.placeholder = ''
-        //             new MutationObserver(() => {
-        //                 if (el.title) {
-        //                     el.title = ''
-        //                 }
-        //                 if (el.placeholder) {
-        //                     el.placeholder = ''
-        //                 }
-        //             }).observe(el, {
-        //                 attributeFilter: ['placeholder', 'title'],
-        //             })
-        //         }
-        //         ++cnt > 20 && clearInterval(id)
-        //     }, 500)
-        // },
-        // enableFnRunAt: 'document-end',
-        description: ['同时禁用 hover 提示与空搜索跳转推荐'],
-        enableFn: () => {
-            liveNavSearchRcmdHider.enable()
+        enableFn: async () => {
+            if (!isPageLive()) {
+                return
+            }
+            if (isRcmdHide) {
+                return
+            }
+            isRcmdHide = true
+            let cnt = 0
+            const id = setInterval(() => {
+                const inputEl = document.querySelector('.search-bar .nav-search-content') as HTMLInputElement
+                const btnEl = document.querySelector('.search-bar .search-btn') as HTMLDivElement
+                if (inputEl && btnEl) {
+                    clearInterval(id)
+                    // 清空数据
+                    inputEl.placeholder = ''
+                    inputEl.title = ''
+                    // 回车事件
+                    inputEl.addEventListener(
+                        'keydown',
+                        (e) => {
+                            if (e.key === 'Enter' && !e.isComposing && inputEl.value.trim() === '') {
+                                e.preventDefault()
+                                e.stopPropagation()
+                                e.stopImmediatePropagation()
+                            }
+                        },
+                        true,
+                    )
+                    // 无输入时单击搜索跳转纯净直播间搜索页
+                    btnEl.addEventListener(
+                        'click',
+                        (e) => {
+                            if (inputEl.value.trim() === '') {
+                                e.preventDefault()
+                                e.stopPropagation()
+                                e.stopImmediatePropagation()
+                                window.open('https://search.bilibili.com/live', '_blank')
+                            }
+                        },
+                        true,
+                    )
+                    // monkey patch title
+                    // placeholder 用纯 CSS 隐藏，B 站不使用 setAttribute 修改 title
+                    const desc = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'title')
+                    if (!desc?.get || !desc?.set) {
+                        return
+                    }
+                    Object.defineProperty(inputEl, 'title', {
+                        get: () => '',
+                        set: () => {},
+                        configurable: true,
+                    })
+                }
+                ++cnt > 20 && clearInterval(id)
+            }, 500)
         },
-        disableFn: () => {
-            liveNavSearchRcmdHider.disable()
-        },
-        enableFnRunAt: 'document-end',
+        enableFnRunAt: 'document-start',
     },
     {
         type: 'switch',

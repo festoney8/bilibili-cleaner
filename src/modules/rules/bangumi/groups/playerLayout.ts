e474547bf5b786c63f2f2f7eb83b63502796e042
festoney8 <festoney8@gmail.com>
2026-09-12 16:45:59 +0800

    fix: fullscreen scrollable eplist menu issue

--- src/modules/rules/bangumi/groups/playerLayout.ts
+++ src/modules/rules/bangumi/groups/playerLayout.ts
@@ -28,6 +28,11 @@ for (const eventName of ['mousewheel', 'DOMMouseScroll', 'wheel']) {
         window,
         eventName,
         (e: WheelEvent) => {
+            // 选集区域允许滚动
+            const target = e.target as HTMLElement
+            if (target.className.startsWith('bpx-player-ctrl-eplist')) {
+                return
+            }
             if (preventVolumeTune && isWebScreen() && !isMiniScreen()) {
                 e.stopImmediatePropagation()
             }
